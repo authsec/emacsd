cd3e2c58080e9c5156e059800844105c01c576e7
Jens Frey <jens.frey@coffeecrew.org>
2021-09-10 18:57:09 +0200

    Add org-roam support

--- init.el
+++ init.el
@@ -147,6 +147,35 @@
   :after org
   :hook (org-mode . org-bullets-mode))
 
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  ;; make sure this directory exists
+  (org-roam-directory "~/roam-notes")
+  ;; configure the folder where dailies are stored, make sure this exists as well
+  (org-roam-dailies-directory "dailies")
+  ;; Lets you use completion-at-point
+  (org-roam-completion-everywhere t)
+  :bind(
+	("C-c n l" . org-roam-buffer-toggle)
+	("C-c n f" . org-roam-node-find)
+	("C-c n i" . org-roam-node-insert)
+	:map org-mode-map
+	("C-M-i" . completion-at-point)
+	:map org-roam-dailies-map
+	("Y" . org-roam-dailies-capture-yesterday)
+	("T" . org-roam-dailies-capture-tomorrow)
+	)
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :config
+  (require 'org-roam-dailies) ;; Ensure keymap is available
+  (org-roam-setup)
+  (org-roam-db-autosync-mode)
+  )
+
 (setq org-latex-pdf-process
       (list
        "docker run --rm -v $\(pwd\):/docs authsec/sphinx /bin/sh -c 'pdflatex -interaction nonstopmode -shell-escape %b.tex && biber %b;  pdflatex -interaction nonstopmode -shell-escape %b.tex && pdflatex -interaction nonstopmode -shell-escape %b.tex'"
